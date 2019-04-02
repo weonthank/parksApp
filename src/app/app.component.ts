3b78830ee6a889016aaaa5bae43e9edd5fd805c2
Will <willieonthank@gmail.com>
2019-04-01 22:56:13 -0700

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,23 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { ParkData } from '../providers/park-data';
+import { ParkData } from './providers/park-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-})
-
-@Component({
-
+  providers: [ParkData]
 })
 
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public parkData: ParkData
   ) {
     this.initializeApp();
+    console.log(parkData.load());
   }
 
   initializeApp() {
@@ -29,5 +28,7 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  
 
 }
